652c92760191c81c279fcf94cb9d19340ae082b4
Luthfi <kankagate@gmail.com>
2025-03-12 13:40:52 +0700

    done useRef with improvements

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,26 @@
+import { useState, useRef } from "react";
 export default function Player() {
+  const playerName = useRef();
+  const [userInput, setUserInput] = useState("unknown entity");
+
+  function handleClick() {
+    if (playerName.current && playerName.current.value.trim() !== "") {
+      setUserInput(playerName.current.value);
+    }
+  }
+
+  function handleKeyDown(e) {
+    if (e.key === "Enter") {
+      handleClick();
+    }
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {userInput}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={playerName} onKeyDown={handleKeyDown} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
