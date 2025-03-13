1ce318f2451d69cd492cce7230a6e8fd650f7b52
Luthfi <kankagate@gmail.com>
2025-03-13 13:00:59 +0700

    add clear textbox after submit

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,25 @@
+import { useState, useRef } from "react";
 export default function Player() {
+  const playerName = useRef(null);
+  const [enteredName, setEnteredName] = useState("unknown entity");
+
+  function handleClick() {
+    if (playerName.current && playerName.current.value.trim() !== "") {
+      setEnteredName(playerName.current.value);
+      playerName.current.value = "";
+    }
+  }
+
+  function handleKeyDown(e) {
+    if (e.key === "Enter") handleClick();
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredName}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={playerName} onKeyDown={handleKeyDown} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
